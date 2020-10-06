ac6a1dd567c7e8c7c235017ad55738ce037ed085
PontusSven <pontus.svensson92@gmail.com>
2020-10-06 11:04:22 +0200

    added video search and display

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
   onFormSubmit = event => {
     event.preventDefault();
 
-    // make sure we call callback from parent component
+    this.props.onTermSubmit(this.state.term);
   };
   render(props) {
     return (
